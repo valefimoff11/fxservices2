2e7ee2b0789539975405baf4a34e1b2963fbe057
valefimoff11 <val.efimoff11@gmail.com>
2025-02-23 19:44:45 +0000

    m

--- telemetry/yyy1.py
+++ telemetry/yyy1.py
@@ -7,16 +7,24 @@ df = pd.DataFrame({
     'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
     'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
 })
+
 df.info()
-print("#####################################################")
+
+print("deep ###############################################")
 df.info(memory_usage='deep')
 
-print("111111111111111#######################################")
+print("deep substring #######################################")
 import io
 buffer = io.StringIO()
 df.info(memory_usage='deep', buf=buffer)
 s = buffer.getvalue()
-print(s)
+print(s[1])
 
 print("#####################################################")
 print(df.memory_usage(True, True))
+
+dmu = df.memory_usage(True, True)
+print(dmu.iloc[0])
+print(dmu.iloc[1])
+print(dmu.iloc[2])
+print(dmu.iloc[3])
