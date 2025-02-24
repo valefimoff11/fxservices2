17fce22a4cb5a5c8d22fe842cba9ca1d374a0748
valefimoff11 <val.efimoff11@gmail.com>
2025-02-24 17:41:41 +0000

    m

--- telemetry/individual_object_profilers.py
+++ telemetry/individual_object_profilers.py
@@ -12,7 +12,7 @@ data_size_suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 def humansize(nbytes):
     i = 0
     while nbytes >= 1024 and i < len(data_size_suffixes)-1:
-        nbytes /= 1024.
+        nbytes /= 1024
         i += 1
     f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
     return '%s %s' % (f, data_size_suffixes[i])
---
+++ telemetry/scan_all_objects.py
@@ -0,0 +1,47 @@
+import sys
+import pandas as pd
+import numpy as np
+
+df2 = pd.DataFrame({
+    'column_1': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
+})
+
+TEST_VAR = 1919
+
+global_vars = globals()
+print(global_vars)
+
+print("####################################################################")
+
+local_vars = locals().copy()
+print(local_vars)
+
+print("####################################################################")
+
+for var, obj in local_vars.items():
+    if isinstance(obj, pd.DataFrame):
+        gso = obj.memory_usage(True, True).sum()
+        print(gso)
+
+sys.exit()
+
+all_vars = global_vars + local_vars
+total = 0
+mem = []
+vars = []
+for var, obj in all_vars:
+    if isinstance(obj, pd.DataFrame):
+        gso = obj.memory_usage().sum()
+    elif isinstance(obj, np.ndarray):
+        gso = obj.size
+    else:
+        gso = sys.getsizeof(obj)
+    total += gso
+    mem = mem + [gso]
+    vars = vars + [var]
+    print(var, gso)
+
+df = pd.DataFrame({'obj': vars, 'size': mem}).sort_values('size', ascending=False)
+print(df['size'].sum()/1000000)
