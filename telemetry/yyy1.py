c0122098c75d7ee22b7017577bdb53f0d39e20bb
valefimoff11 <val.efimoff11@gmail.com>
2025-02-24 00:13:12 +0000

    m

--- telemetry/yyy1.py
+++ telemetry/yyy1.py
@@ -11,6 +11,12 @@ df = pd.DataFrame({
     'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
 })
 
+df2 = pd.DataFrame({
+    'column_1': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
+})
+
 logging.basicConfig(level=logging.DEBUG)
 logging.info("logging starts")
 
@@ -20,6 +26,9 @@ df.info()
 print("###############################################")
 
 print( round( sys.getsizeof(df)/1024/1024, 1) )
+
+p = {"a" : df, "b" : df2}
+print( round( sys.getsizeof(p)/1024/1024, 1) )
 
 print("deep ###############################################")
 
