0e6a2783a91d178849c586360f514d3fb0db0039
valefimoff11 <val.efimoff11@gmail.com>
2025-02-24 20:52:57 +0000

    m

---
+++ telemetry/trace_malloc_profiler_driver3.py
@@ -0,0 +1,42 @@
+import linecache
+import os
+import tracemalloc
+
+import numpy as np
+import pandas as pd
+
+
+def display_top(snapshot, key_type='lineno', limit=10):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, frame.filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+tracemalloc.start()
+
+# ... run your application ...
+df1 = pd.DataFrame({
+    'column_1': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
+})
+
+snapshot = tracemalloc.take_snapshot()
+display_top(snapshot)
