1db5a0433118f8f5da08892af0ac229a15bba900
valefimoff11 <val.efimoff11@gmail.com>
2025-02-25 18:19:16 +0000

    m

---
+++ telemetry/guppy_profiler.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from guppy import hpy
+
+h=hpy()
+
+df1 = pd.DataFrame({
+    'column_1': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
+})
+
+print(h.heap())
+
+df2 = pd.DataFrame({
+    'column_1': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_2': np.random.choice(['a', 'b', 'c'], 10 ** 6),
+    'column_3': np.random.choice(['a', 'b', 'c'], 10 ** 6)
+})
+
+print(h.heap())
